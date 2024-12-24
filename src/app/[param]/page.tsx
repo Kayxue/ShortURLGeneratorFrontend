d1dd0b738cf7e8b271110e5ec88a15992014c1ae
Kayxue <s0990006@gmail.com>
2024-12-24 14:33:44 +0800

    formating

--- src/app/[param]/page.tsx
+++ src/app/[param]/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-
-import React,{ useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function Page({ params }: { params: Promise<{ param: string }> }) {
 	const [password, setPassword] = useState("");
